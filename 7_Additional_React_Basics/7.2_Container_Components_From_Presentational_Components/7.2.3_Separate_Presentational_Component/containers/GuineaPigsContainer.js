768d8401bc6a5645f63e12f144d17ac528be2b9e
formidablae <81068781+formidablae@users.noreply.github.com>
2022-09-26 02:39:16 +0200

    Did 7 Additional React Basics / 7.2 Container Components From Presentational Compoenents

---
+++ 7_Additional_React_Basics/7.2_Container_Components_From_Presentational_Components/7.2.3_Separate_Presentational_Component/containers/GuineaPigsContainer.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { GuineaPigs } from '../components/GuineaPigs';
+
+const GUINEAPATHS = [
+    'https://content.codecademy.com/courses/React/react_photo-guineapig-1.jpg',
+    'https://content.codecademy.com/courses/React/react_photo-guineapig-2.jpg',
+    'https://content.codecademy.com/courses/React/react_photo-guineapig-3.jpg',
+    'https://content.codecademy.com/courses/React/react_photo-guineapig-4.jpg'
+];
+
+class GuineaPigs extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { currentGP: 0 };
+
+        this.interval = null;
+
+        this.nextGP = this.nextGP.bind(this);
+    }
+
+    nextGP() {
+        let current = this.state.currentGP;
+        let next = ++current % GUINEAPATHS.length;
+        this.setState({ currentGP: next });
+    }
+
+    componentDidMount() {
+        this.interval = setInterval(this.nextGP, 5000);
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.interval);
+    }
+
+    render() {
+        let src = GUINEAPATHS[this.state.currentGP];
+        return (
+            <div>
+                <h1>Cute Guinea Pigs</h1>
+                <img src={src} />
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(
+    <GuineaPigs />,
+    document.getElementById('app')
+);
---
+++ 7_Additional_React_Basics/7.2_Container_Components_From_Presentational_Components/7.2.4_Render_Presentational_Component_in_Container_Component/containers/GuineaPigsContainer.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { GuineaPigs } from '../components/GuineaPigs';
+
+const GUINEAPATHS = [
+    'https://content.codecademy.com/courses/React/react_photo-guineapig-1.jpg',
+    'https://content.codecademy.com/courses/React/react_photo-guineapig-2.jpg',
+    'https://content.codecademy.com/courses/React/react_photo-guineapig-3.jpg',
+    'https://content.codecademy.com/courses/React/react_photo-guineapig-4.jpg'
+];
+
+class GuineaPigsContainer extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { currentGP: 0 };
+
+        this.interval = null;
+
+        this.nextGP = this.nextGP.bind(this);
+    }
+
+    nextGP() {
+        let current = this.state.currentGP;
+        let next = ++current % GUINEAPATHS.length;
+        this.setState({ currentGP: next });
+    }
+
+    componentDidMount() {
+        this.interval = setInterval(this.nextGP, 5000);
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.interval);
+    }
+
+    render() {
+        let src = GUINEAPATHS[this.state.currentGP];
+        return <GuineaPigs src={src} />;
+    }
+}
+
+ReactDOM.render(
+    <GuineaPigsContainer />,
+    document.getElementById('app')
+);
